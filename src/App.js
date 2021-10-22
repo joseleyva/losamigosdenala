5b81fd87e9b7bbb84b0679b35c17b03bdb5744c5
joseleyva <17040457@itdurango.edu.mx>
2021-10-22 16:29:39 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
        
-      <img src="Nala.jpg" alt="" height="auto" width="auto"/>
+     
     
       </header>
+      <img src="Nala.jpg" alt="" height="auto" width="auto"/>
      <h4>Esto es una prueba de la pagina los amigos de Nala</h4>
     </div>
   );
