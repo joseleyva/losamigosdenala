c3107af35c16cb147478b7bef26fbfb9f2ddb4e6
joseleyva <17040457@itdurango.edu.mx>
2021-10-25 22:48:10 -0500

    Update Principal

--- src/App.js
+++ src/App.js
@@ -1,35 +1,98 @@
-
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar,Container, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap';
+import {Navbar,Container, Nav, Form, FormControl, Button} from 'react-bootstrap';
+import * as React from 'react';
+import Grid from '@mui/material/Grid';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import MainFeaturedPost from './MainFeaturedPost';
+import FeaturedPost from './FeaturedPost';
+import Main from './Main';
+import Sidebar from './Sidebar';
+import post1 from './blog-post.1.md';
+import post2 from './blog-post.2.md';
+import post3 from './blog-post.3.md';
+
+
+
+const mainFeaturedPost = {
+  title: 'Title of a longer featured blog post',
+  description:
+    "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+  image: 'https://source.unsplash.com/random',
+  imageText: 'main image description',
+  linkText: 'Continue reading…',
+};
+
+const featuredPosts = [
+  {
+    title: 'Featured post',
+    date: 'Nov 12',
+    description:
+      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+    image: 'https://source.unsplash.com/random',
+    imageLabel: 'Image Text',
+  },
+  {
+    title: 'Post title',
+    date: 'Nov 11',
+    description:
+      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+    image: 'https://source.unsplash.com/random',
+    imageLabel: 'Image Text',
+  },
+];
+
+const posts = [post1, post2, post3];
+
+const sidebar = {
+  title: 'About',
+  description:
+    'Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.',
+  archives: [
+    { title: 'March 2020', url: '#' },
+    { title: 'February 2020', url: '#' },
+    { title: 'January 2020', url: '#' },
+    { title: 'November 1999', url: '#' },
+    { title: 'October 1999', url: '#' },
+    { title: 'September 1999', url: '#' },
+    { title: 'August 1999', url: '#' },
+    { title: 'July 1999', url: '#' },
+    { title: 'June 1999', url: '#' },
+    { title: 'May 1999', url: '#' },
+    { title: 'April 1999', url: '#' },
+  ],
+  social: [
+    
+  ],
+};
+
+const theme = createTheme();
+
 
 function App() {
   return (
     <div className="App">
       <header >
-      <Navbar bg="light" expand="lg">
+      <Navbar className="Barra">
   <Container fluid>
-    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+  <Navbar.Brand href="/">
+        <img
+          alt=""
+          src="IconNala.png"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{' '}
+      Los Amigos de Nala
+      </Navbar.Brand>
     <Navbar.Toggle aria-controls="navbarScroll" />
     <Navbar.Collapse id="navbarScroll">
       <Nav
-        className="me-auto my-2 my-lg-0"
-        style={{ maxHeight: '100px' }}
-        navbarScroll
+        className="me-auto my-2 my-lg-0 NavB"
       >
-        <Nav.Link href="#action1">Home</Nav.Link>
-        <Nav.Link href="#action2">Link</Nav.Link>
-        <NavDropdown title="Link" id="navbarScrollingDropdown">
-          <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action5">
-            Something else here
-          </NavDropdown.Item>
-        </NavDropdown>
-        <Nav.Link href="#" disabled>
-          Link
-        </Nav.Link>
+        <Nav.Link href="">Mascotas</Nav.Link>
+        <Nav.Link href="">Contacto</Nav.Link>
+        <Nav.Link href="">Acerca de</Nav.Link>
       </Nav>
       <Form className="d-flex">
         <FormControl
@@ -38,16 +101,36 @@ function App() {
           className="me-2"
           aria-label="Search"
         />
-        <Button variant="outline-success">Search</Button>
+        <Button variant="primary">Search</Button>
       </Form>
     </Navbar.Collapse>
   </Container>
 </Navbar>
-     
-    
       </header>
-      <img src="Nala.jpg" alt="" height="auto" width="auto"/>
-     <h4>Esto es una prueba de la pagina los amigos de Nala</h4>
+      
+      <ThemeProvider theme={theme}>
+      <Container maxWidth="lg">
+        <main>
+          <MainFeaturedPost post={mainFeaturedPost} />
+          <Grid container spacing={4}>
+            {featuredPosts.map((post) => (
+              <FeaturedPost key={post.title} post={post} />
+            ))}
+          </Grid>
+          <Grid container spacing={5} sx={{ mt: 3 }}>
+            <Main title="From the firehose" posts={posts} />
+            <Sidebar
+              title={sidebar.title}
+              description={sidebar.description}
+              archives={sidebar.archives}
+              social={sidebar.social}
+            />
+          </Grid>
+        </main>
+      </Container>
+    </ThemeProvider>
+
+
     </div>
   );
 }
